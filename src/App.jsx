13a158e111c3be515a9a46c97192842b5321b6ba
dmychel <dmychel@tutanota.com>
2024-12-28 17:49:13 -0500

    change theme
    moved arrays to a seperate js file. created a function that toggles usestate of active array.

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,14 @@
+import { useState } from "react";
+import { sonicArr, linkArr } from "./data";
 import "./App.css";
 
 function App() {
-  const cvDataArr = [
-    {
-      company: "Savior of Hyrule",
-      jobs: [
-        {
-          position: "Hero of Twilight",
-          employment: "2006 – Present",
-          summary:
-            "A courageous and resourceful adventurer with a proven track record of rescuing kingdoms, solving complex problems, and facing dark forces.",
-          PAR: [
-            "Led the successful rescue of Princess Zelda, defeating the evil sorcerer Zant and ultimately restoring peace to Hyrule.",
-            "Wielded the Master Sword, an ancient weapon with the power to vanquish evil, to defeat Ganondorf and his minions across multiple dimensions and realms.",
-            "Went snowboarding with kind Yetis",
-            "Midna's sidekick :')",
-          ],
-        },
-        {
-          position: "Hero of Winds",
-          employment: "2002 – Present",
-          summary:
-            "An adaptable and brave adventurer with a proven history of saving the Great Sea from the clutches of evil.",
-          PAR: [
-            "Sailing & Navigation: Expert in the art of seafaring, capable of navigating vast oceans and navigating through treacherous waters with precision.",
-            "Set sail across the Great Sea to locate the lost pieces of the Triforce of Courage and restore peace to the islands.",
-            "Formed key alliances with the Pirate Crew, the Sages, and the mysterious King of Red Lions, who guided and supported during perilous missions.",
-          ],
-        },
-      ],
-    },
-    {
-      company: "Hyrules most admired man",
-      jobs: [
-        {
-          position: "Suitor",
-          employment: "1998 - PRES",
-          summary:
-            "For some reason, across all the lands, across all the time lines, I have become the most sought after man in Hyrule",
-          PAR: [
-            "Malon (Ocarina of Time): Expresses clear affection, gifts Link Epona as a token of love.",
-            "Saria (Ocarina of Time): Childhood friend with a strong emotional connection, deep care and admiration.",
-            "Midna (Twilight Princess): Strong, emotional attachment that evolves from ally to something deeper, ending in bittersweet farewell.",
-          ],
-        },
-      ],
-    },
-  ];
+  const [arr, setArr] = useState(linkArr);
+  const array = arr;
+
+  function handleClick() {
+    setArr((prevArray) => (prevArray === linkArr ? sonicArr : linkArr));
+  }
 
   return (
     <div className="app">
@@ -59,12 +21,13 @@ function App() {
           }}
         >
           <h1 style={{ margin: ".5em 0" }}>Resume</h1>{" "}
-          <button style={{ padding: "0.25rem 1rem" }}>Change Theme</button>
+          <button style={{ padding: "0.25rem 1rem" }} onClick={handleClick}>
+            Change Theme
+          </button>
         </div>
 
         <div className="cv">
-          {cvDataArr.map((obj, index) => {
-            console.log(obj);
+          {array.map((obj, index) => {
             return (
               <div key={index} className="job">
                 <span className="top_span rotate">
