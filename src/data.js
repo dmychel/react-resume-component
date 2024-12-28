a6188180a8edff8edcb6b45f2224eb1470353f5c
dmychel <dmychel@tutanota.com>
2024-12-28 17:49:58 -0500

    created file to hold array data

---
+++ src/data.js
@@ -0,0 +1,79 @@
+const linkArr = [
+  {
+    company: "Savior of Hyrule",
+    jobs: [
+      {
+        position: "Hero of Twilight",
+        employment: "2006 – Present",
+        summary:
+          "A courageous and resourceful adventurer with a proven track record of rescuing kingdoms, solving complex problems, and facing dark forces.",
+        PAR: [
+          "Led the successful rescue of Princess Zelda, defeating the evil sorcerer Zant and ultimately restoring peace to Hyrule.",
+          "Wielded the Master Sword, an ancient weapon with the power to vanquish evil, to defeat Ganondorf and his minions across multiple dimensions and realms.",
+          "Went snowboarding with kind Yetis",
+          "Midna's sidekick :')",
+        ],
+      },
+      {
+        position: "Hero of Winds",
+        employment: "2002 – Present",
+        summary:
+          "An adaptable and brave adventurer with a proven history of saving the Great Sea from the clutches of evil.",
+        PAR: [
+          "Sailing & Navigation: Expert in the art of seafaring, capable of navigating vast oceans and navigating through treacherous waters with precision.",
+          "Set sail across the Great Sea to locate the lost pieces of the Triforce of Courage and restore peace to the islands.",
+          "Formed key alliances with the Pirate Crew, the Sages, and the mysterious King of Red Lions, who guided and supported during perilous missions.",
+        ],
+      },
+    ],
+  },
+  {
+    company: "Hyrules most admired man",
+    jobs: [
+      {
+        position: "Suitor",
+        employment: "1998 - PRES",
+        summary:
+          "For some reason, across all the lands, across all the time lines, I have become the most sought after man in Hyrule",
+        PAR: [
+          "Malon (Ocarina of Time): Expresses clear affection, gifts Link Epona as a token of love.",
+          "Saria (Ocarina of Time): Childhood friend with a strong emotional connection, deep care and admiration.",
+          "Midna (Twilight Princess): Strong, emotional attachment that evolves from ally to something deeper, ending in bittersweet farewell.",
+        ],
+      },
+    ],
+  },
+];
+
+const sonicArr = [
+  {
+    company: "Defender of Earth and Friends",
+    jobs: [
+      {
+        position: "Adventurer",
+        employment: "1998 – Present",
+        summary:
+          "High-energy, fearless, and super sonic fast hero with a proven track record of saving the world and stopping evil forces.",
+        PAR: [
+          "LIVE AND LEARN",
+          "Defended earth from Dr. Robotnik’s attempt to harness the power of Chaos, a destructive force capable of wiping out entire cities.",
+          "Took on the challenge of defeating Chaos, saving Station Square and the future of earth.",
+        ],
+      },
+      {
+        position: "Adventurer 2",
+        employment: "2001 – Present",
+        summary:
+          "An adaptable and brave adventurer with a proven history of saving the Great Sea from the clutches of evil.",
+        PAR: [
+          "Rolling around at the speed of sound",
+          "Down in pumpkin hill I gots to find my lost piece",
+          "Stopped Dr. Robotnik's evil plot to use the Ultimate Lifeform, Shadow, and the Chaos Emeralds to destroy Earth.",
+          "Joined forces with allies and rival characters like Shadow to thwart the Bio-Lizard and stop a global catastrophe.",
+        ],
+      },
+    ],
+  },
+];
+
+export { linkArr, sonicArr };
