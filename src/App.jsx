6cb757a1757d1624ef37a4c78630b5ae1c933d4a
dmychel <dmychel@tutanota.com>
2024-12-28 16:46:26 -0500

    channged array object properties

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,45 @@ import "./App.css";
 function App() {
   const cvDataArr = [
     {
-      company: "Website Designer",
+      company: "Savior of Hyrule",
       jobs: [
         {
-          position: "Independent Contractor",
-          employment: "MAY 2023 - PRES",
+          position: "Hero of Twilight",
+          employment: "2006 – Present",
           summary:
-            "I provide a simplistic yet effective approach to web design, focusing first on the user, with a simple design and ease of use. I also offer infrastructure development to track site traffic, performance, and anything else the customer needs to be successful.",
+            "A courageous and resourceful adventurer with a proven track record of rescuing kingdoms, solving complex problems, and facing dark forces.",
           PAR: [
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
+            "Led the successful rescue of Princess Zelda, defeating the evil sorcerer Zant and ultimately restoring peace to Hyrule.",
+            "Wielded the Master Sword, an ancient weapon with the power to vanquish evil, to defeat Ganondorf and his minions across multiple dimensions and realms.",
+            "Went snowboarding with kind Yetis",
+            "Midna's sidekick :')",
+          ],
+        },
+        {
+          position: "Hero of Winds",
+          employment: "2002 – Present",
+          summary:
+            "An adaptable and brave adventurer with a proven history of saving the Great Sea from the clutches of evil.",
+          PAR: [
+            "Sailing & Navigation: Expert in the art of seafaring, capable of navigating vast oceans and navigating through treacherous waters with precision.",
+            "Set sail across the Great Sea to locate the lost pieces of the Triforce of Courage and restore peace to the islands.",
+            "Formed key alliances with the Pirate Crew, the Sages, and the mysterious King of Red Lions, who guided and supported during perilous missions.",
           ],
         },
       ],
     },
     {
-      company: "Website Designer",
+      company: "Hyrules most admired man",
       jobs: [
         {
-          position: "Independent Contractor",
-          employment: "MAY 2023 - PRES",
+          position: "Suitor",
+          employment: "1998 - PRES",
           summary:
-            "I provide a simplistic yet effective approach to web design, focusing first on the user, with a simple design and ease of use. I also offer infrastructure development to track site traffic, performance, and anything else the customer needs to be successful.",
+            "For some reason, across all the lands, across all the time lines, I have become the most sought after man in Hyrule",
           PAR: [
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-          ],
-        },
-      ],
-    },
-    {
-      company: "Website Designer",
-      jobs: [
-        {
-          position: "Independent Contractor",
-          employment: "MAY 2023 - PRES",
-          summary:
-            "I provide a simplistic yet effective approach to web design, focusing first on the user, with a simple design and ease of use. I also offer infrastructure development to track site traffic, performance, and anything else the customer needs to be successful.",
-          PAR: [
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi aspernatur",
+            "Malon (Ocarina of Time): Expresses clear affection, gifts Link Epona as a token of love.",
+            "Saria (Ocarina of Time): Childhood friend with a strong emotional connection, deep care and admiration.",
+            "Midna (Twilight Princess): Strong, emotional attachment that evolves from ally to something deeper, ending in bittersweet farewell.",
           ],
         },
       ],
@@ -61,8 +57,12 @@ function App() {
             console.log(obj);
             return (
               <div key={index} className="job">
-                <span className="top_span rotate"></span>
-                <span className="bottom_span rotate"></span>
+                <span className="top_span rotate">
+                  <img src="/public/icons/triforce.png" alt="" />
+                </span>
+                <span className="bottom_span rotate">
+                  <img src="/public/icons/triforce.png" alt="" />
+                </span>
                 <h3>{obj.company}</h3>
                 {obj.jobs.map((job, index) => {
                   return (
@@ -91,10 +91,12 @@ function App() {
             );
           })}
         </div>
-        <span>
-          Do you Like this resume layout? You can see the code{" "}
-          <a href="#">here!</a>
-        </span>
+        <div className="cot">
+          <p>
+            Do you Like this resume layout? You can see the code{" "}
+            <a href="#">here!</a>
+          </p>
+        </div>
       </div>
     </div>
   );
